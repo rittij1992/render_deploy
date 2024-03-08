fa72a8ad1069be01eddfdc955ffa27e1c07baa41
rittij1992 <rittij.mazumder@gmail.com>
2024-03-08 23:20:11 +0530

    databse inpmi

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose  = require("mongoose");
 
-mongoose.connect("").
+mongoose.connect("mongodb+srv://rittijmazumder:iam-rittij1992@cluster0.rwttnzo.mongodb.net/new_db").
 then((data)=>{
-    console.log();
+    console.log("database connected");
 }).catch((err)=>{
     console.log(err);
 })
@@ -15,6 +15,25 @@ const server = express();
 server.get("/", (req, res)=>{
    res.send( "this app server")
 });
+
+server.get("/roles", async (req, res)=> {
+    const roleSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            enum: ["admin", "customer"],
+            default: "customer"
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    })
+    
+    const Role = mongoose.model("Role", roleSchema);
+
+    const data = await Role.find();
+    res.json({data});
+})
 
 server.get("/new", (req, res)=>{
     res.send( "this is new page")
